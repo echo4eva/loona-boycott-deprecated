31f016060e4266006fdcc5612c7b605615a5b354
Jericho <perilecho@gmail.com>
2024-05-20 18:33:47 -0700

    boycott with token

--- backend/auth/__init__.py
+++ backend/auth/__init__.py
@@ -1,5 +1,5 @@
 # backend/auth/__init__.py
-from .index import get_spotify_client, get_spotify_auth
+from .index import get_spotify_client, get_spotify_auth, get_token
 
 spotify_client = get_spotify_client()
 spotify_auth = get_spotify_auth()
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 # backend\main.py
 from flask import Flask, redirect, request, jsonify, session
-from auth import get_spotify_auth
+from auth import get_spotify_auth, get_token
 from dotenv import load_dotenv
-
+from utils.replace import replace_songs_with_ep
 load_dotenv()
 import os
+import spotipy
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
@@ -29,6 +30,35 @@ def call_back():
     session['TOKEN_INFO'] = token_info
     
     return session['TOKEN_INFO']
+
+@app.route('/me')
+def test_token():
+    access_token = get_token()
+    if access_token:
+        sp = spotipy.Spotify(auth=access_token)
+        user_info = sp.me()
+        return jsonify(user_info), 200
+    else:
+        return jsonify({'error' : 'failed to get access token'}), 401
+    
+@app.route('/boycott', methods=['POST'])
+def boycott_playlist():
+    access_token = get_token()
+    if access_token:
+        playlist_id = request.form.get('playlist_id')
+        print(playlist_id)
+        if playlist_id:
+            try:
+                replace_songs_with_ep(playlist_id)
+                return jsonify({'message': 'Playlist updated successfully'}), 200
+            except Exception as e:
+                return jsonify({'error': str(e)}), 500
+        else:
+            return jsonify({'error': 'Playlist ID is required'}), 400
+    else:
+        return jsonify({'error': 'Failed to retrieve access token'}), 401
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
